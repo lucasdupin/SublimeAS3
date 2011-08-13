8ad6eaabcf5fe3a1d43fa460c2f788b0b64581f0
Jan Cássio <janio@jancassio.com>
2011-08-13 12:32:25 -0300

    use quick panel to display a list of contextual import class options

--- actionscript3_import.py
+++ actionscript3_import.py
@@ -12,6 +12,35 @@ class Actionscript3Import(sublime_plugin.TextCommand):
 		current_word = self.view.substr(current_word_region)
 
 		matches = st.find_package(current_word.strip())
+		
+		def on_choose_class(index):
+				
+				# Nothing selected
+				if index == -1:
+					return
+				
+				# Class name
+				imp = matches[index];
+
+				cla = imp.split('.').pop()
+				# Replace Class name
+				self.view.replace(edit, current_word_region, cla)
+
+				# Exit if already imported
+				if self.view.find("^\\s*import\\s+%s(\\s|;)" % imp.replace('.', '\\.'), 0):
+					return
+				# Searching where to add
+				pkg = self.view.find("^\\s*package\\b\\s*([\\w+\\.]*)[\\s\\n]*\\{", 0)
+				# cls = self.view.find("^\\s*(public|final)\\s+(final|public)?\\s*\\b(class|interface|function)\\b", 0)
+				# mta = self.view.find_all("^\\s*\\[(Style|Bindable|Event|Embed|SWF)")
+				# Calculate
+				# insert_before = (mta+[cls])[0] # Before class or meta
+				insert_after = sublime.Region(0, 0) if pkg is None else pkg # after the package, if it exists
+
+				l = self.view.line(insert_after.end())
+				self.view.replace(edit, l, "%s\n\timport %s;" % (self.view.substr(l), imp));
+
+				self.view.show(self.view.sel()[0])
 
 		if len(matches) == 0:
 			sublime.status_message("No package found");
@@ -20,24 +49,4 @@ class Actionscript3Import(sublime_plugin.TextCommand):
 			imp = matches[0]
 		else:
 			# Lots of choices
-			imp = matches[0] # TODO
-		# Class name
-		cla = imp.split('.').pop()
-		# Replace Class name
-		self.view.replace(edit, current_word_region, cla)
-
-		# Exit if already imported
-		if self.view.find("^\\s*import\\s+%s(\\s|;)" % imp.replace('.', '\\.'), 0):
-			return
-		# Searching where to add
-		pkg = self.view.find("^\\s*package\\b\\s*([\\w+\\.]*)[\\s\\n]*\\{", 0)
-		# cls = self.view.find("^\\s*(public|final)\\s+(final|public)?\\s*\\b(class|interface|function)\\b", 0)
-		# mta = self.view.find_all("^\\s*\\[(Style|Bindable|Event|Embed|SWF)")
-		# Calculate
-		# insert_before = (mta+[cls])[0] # Before class or meta
-		insert_after = sublime.Region(0, 0) if pkg is None else pkg # after the package, if it exists
-
-		l = self.view.line(insert_after.end())
-		self.view.replace(edit, l, "%s\n\timport %s;" % (self.view.substr(l), imp));
-
-		self.view.show(self.view.sel()[0])
+			sublime.active_window().show_quick_panel(matches, on_choose_class);
